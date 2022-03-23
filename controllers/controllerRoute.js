940cfd4607b35d0c70f7c7346d524ad8703a1bfe
Francis Bolduc <f.bolduc@outlook.com>
2022-03-23 16:50:31 -0400

    premiere version avec spot et creation de spot

--- controllers/controllerRoute.js
+++ controllers/controllerRoute.js
@@ -2,30 +2,11 @@ const axios = require("axios");
 const SkiApi = "https://ski-api.herokuapp.com";
 
 exports.getSignup = (req, res) => {
-    res.render('signup', {
-        signupFaile: undefined
-    });
+    res.render('signup', {signupFaile: undefined});
 };
 
 exports.getLogin = (req, res) => {
-    res.render('login', {
-        loginFaile: undefined
-    });
-};
-
-exports.getCreateSpot = (req, res) => {
-    let token = res.app.locals.apiKey;
-    if(token){
-        res.render("creeSpot", { createFaile : undefined });
-    }
-    else{res.render("login", { loginFaile : undefined });}
-};
-
-exports.getDeconnexion = (req, res) => {
-    res.app.locals.apiKey = "";
-    res.render('login', {
-        loginFaile: undefined
-    });
+    res.render('login', {loginFaile: undefined});
 };
 
 exports.getProfil = (req, res) => {
@@ -47,25 +28,6 @@ exports.getProfil = (req, res) => {
     } else {
         res.render("login", {loginFaile: undefined});
     }
-};
-
-exports.postSignup = (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-    let name = req.body.name;
-    axios.post(SkiApi + "/signup", {
-            "email": email,
-            "password": password,
-            "name": name
-        })
-        .then(resultat => {
-            res.render('login');
-        })
-        .catch(erreur => {
-            res.render("signup", {
-                signupFaile: "Compte déjas existant"
-            });
-        });
 };
 
 exports.postLogin = (req, res) => {
@@ -93,42 +55,67 @@ exports.postLogin = (req, res) => {
 };
 
 
+exports.postSignup = (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let name = req.body.name;
+    axios.post(SkiApi + "/signup", {
+            "email": email,
+            "password": password,
+            "name": name
+        })
+        .then(resultat => {
+            res.render('login');
+        })
+        .catch(erreur => {
+            res.render("signup", {
+                signupFaile: "Compte déjas existant"
+            });
+        });
+};
+
 exports.getSpot = (req, res) => {
     let token = res.app.locals.apiKey;
 
-    axios.get(SkiApi + "/ski-spot", {
-
-            headers: {"Authorization": token}
-        })
+    axios.get(SkiApi + "/ski-spot", {headers: {"Authorization": token}})
         .then(resultat => {
             spots = resultat.data.skiSpots;
 
-            axios.get(SkiApi + "/tokenInfo", {
-
-                    headers: {"Authorization": token}
-                })
+            axios.get(SkiApi + "/tokenInfo", {headers: {"Authorization": token}})
                 .then(resultat => {
                     let name = resultat.data.name;
 
-                    res.render('spot', { spots, name, deleteFaile : undefined });
+                    res.render('testSpot', { spots, name, deleteFaile : undefined });
                 })
         }).catch(erreur => {
             res.render("login", { loginFaile: undefined });
         });
 };
 
+exports.getCreateSpot = (req, res) => {
+    let token = res.app.locals.apiKey;
+    if(token){
+        res.render("testCreate", { createFaile : undefined });
+    }
+    else{res.render("login", { loginFaile : undefined });}
+};
+
 exports.postCreateSpot = (req, res) => {
     let token = res.app.locals.apiKey;
     let description = req.body.description;
     let name = req.body.name;
+    let address = req.body.address;
+    let difficulty = req.body.difficulty;
+    let coordinates = req.body.description;
+
 
     axios.post(SkiApi + "/ski-spot", {
 
             "description": description,
             "name": name,
-            "address": "test",
-            "difficulty": "facile",
-            "coordinates": [1, 2]
+            "address": address,
+            "difficulty": difficulty,
+            "coordinates": [1,2]
         }, 
 
         {headers: {"Authorization": token,}})
@@ -137,7 +124,7 @@ exports.postCreateSpot = (req, res) => {
             res.redirect('/spot');
         })
         .catch(erreur => {
-            res.render("creeSpot", {createFaile : "Problème avec la création de Spot"});
+            res.render("testCreate", {createFaile : "Problème avec la création de Spot"});
         });
 };
 
@@ -154,4 +141,11 @@ exports.deleteSpot = (req, res) => {
         }).catch(erreur => {
             res.render({deleteFaile : "Problème dans la suppression du Spot"});
         });
+};
+
+exports.getDeconnexion = (req, res) => {
+    res.app.locals.apiKey = "";
+    res.render('login', {
+        loginFaile: undefined
+    });
 };
