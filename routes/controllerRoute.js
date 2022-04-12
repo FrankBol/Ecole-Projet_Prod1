658a5e0f0c7869eb4620ba34a861eef869c0875c
Francis Bolduc <f.bolduc@outlook.com>
2022-04-12 01:03:34 -0400

    Gros changement dans le Controller

--- routes/controllerRoute.js
+++ routes/controllerRoute.js
@@ -1,239 +1,154 @@
 const axios = require("axios");
 const SkiApi = "https://ski-api.herokuapp.com";
 
-exports.getSignup = (req, res) => {
-    res.render('signup');
-};
+module.exports = {
 
-exports.getLogin = (req, res) => {
-    res.redirect("/");    
-};
+    pageSpotsList : (req, res) => {
+        res.render('spots_list');
+    },
 
-exports.getDeconnexion = (req, res) => {
-    res.app.locals.apiKey = "";
-    res.redirect("/");
-};
+    pageSignup : (req, res) => {
+        res.render('signup');
+    },
 
-exports.getProfil = (req, res) => {
-    if (res.app.locals.apiKey) {
-        let token = res.app.locals.apiKey;
+    pageProfil : (req, res) => {
+        res.render('profil');
+    },
+
+    pageCreateSpot : (req, res) => {
+        res.render('spot_creation');
+    },
+
+    pageInformation : (req, res) => {
+        res.render('spot_information');
+    },
+
+    pageUpdate : (req, res) => {
+        res.render('spot_update');
+    },
+
+    getDeconnecxion : (req, res) => {
+        res.app.locals.apiKey = "";
+        res.redirect("/");
+    },
+
+    postSignup : (req, res) => {
+        let infoSignup = req.body;
+    
+        axios.post(SkiApi + "/signup", infoSignup )
+        .then( () => res.redirect("/"))
+    
+        .catch(erreur => {
+            req.flash("error", `Ce compte existe déjà`);
+            res.redirect("/users/signup");
+        });
+    },
+
+    postLogin : (req, res) => {
+        let infoLogin = req.body;
+    
+        axios.post(SkiApi + "/login", infoLogin)
+            .then(resultat => {
+                res.app.locals.apiKey = resultat.data.token;
+                res.redirect('/');
+            })
+            .catch(() => {
+                req.flash("error", `Mauvais Mot de Passe ou Email`);
+                res.redirect('/');
+            });
+    },
+
+    getProfil : (req, res, next) => {
+        let token = res.app.locals.apiKey;  
 
         axios.get(SkiApi + "/tokenInfo", {headers: {"Authorization": token}})
+        .then(resultat => {
+            res.app.locals.infoProfil = resultat.data;
+            next();
+        })
+        .catch(() => {res.redirect("/");});
+    },
 
+    getSpots : (req, res, next) => {
+        let token = res.app.locals.apiKey;  
+        let page = req.query.page;
+        if (page==undefined) page=1;
+    
+        axios.get(SkiApi + `/ski-spot?limit=6&page=${page}`, {headers: {"Authorization": token}})
             .then(resultat => {
-                let data = resultat.data;
-                res.render('profil', {name: data.name, email: data.email});
+                res.locals.spots = resultat.data.skiSpots;
+                res.locals.totalPages = resultat.data.totalPages;
+                res.locals.page = page;
+                next();
 
+            }).catch(() => {
+                res.render("login");
+            });
+    },
+    postCreateSpot : (req, res) => {
+        let token = res.app.locals.apiKey;
+        let infoCreate = req.body;
+    
+        let coordinates =  req.body.coordinates;
+        let tabCoordinates = coordinates.split(",");
+        let numberTabCoordinates = tabCoordinates.map( i => Number(i));
+    
+        axios.post(SkiApi + "/ski-spot", {
+                ...infoCreate,
+                "coordinates": numberTabCoordinates
+            }, 
+            {headers: {"Authorization": token}})
+    
+            .then(() => res.redirect('/'))
+            .catch(() => {
+                req.flash("error", `Impossible de créer le spot`);
+                res.redirect("/spots/create");
+            });
+    },
+    getSpotInformation : (req, res, next) => {
+        let token = res.app.locals.apiKey;
+        let id = req.params.id;
+    
+        axios.get(SkiApi + "/ski-spot/" + id, {headers: {"Authorization": token}})
+            .then(resultat => {
+                res.app.locals.info = resultat.data.skiSpot;
+                next();
             })
             .catch(() => {res.redirect("/");});
-
-    } else {res.redirect("/");}
-};
-
-exports.postSignup = (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-    let name = req.body.name;
-
-    axios.post(SkiApi + "/signup", 
-        {
-            "email": email,
-            "password": password,
-            "name": name
-        })
-    .then(resultat => {res.redirect("/");})
-
-    .catch(erreur => {
-        req.flash("error", `Ce compte existe déjà`);
-        res.redirect("/users/signup");
-    });
-};
-
-exports.postLogin = (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-
-    axios.post(SkiApi + "/login", 
-        {
-            "email": email,
-            "password": password,
-        })
-        .then(resultat => {
-            let data = resultat.data;
-            res.app.locals.apiKey = data.token;
-
-            res.redirect('/');
-        })
-        .catch(() => {
-            req.flash("error", `Mauvais Mot de Passe ou Email`);
-            res.redirect('/');
-        });
-};
-
-exports.getSpots = (req, res) => {
-    let token = res.app.locals.apiKey;
-
-    let page = req.query.page;
-    if (page==undefined) page=1;
-
-    axios.get(SkiApi + `/ski-spot?limit=6&page=${page}`, {headers: {"Authorization": token}})
-        .then(resultat => {
-            let spots = resultat.data.skiSpots;
-            let totalPages = resultat.data.totalPages;
-            // console.log(`resultat ${JSON.stringify(resultat.data)}`);
-            axios.get(SkiApi + "/tokenInfo", {headers: {"Authorization": token}})
-                .then(resultat => {
-                    let name = resultat.data.name;
-                    res.render('spots_list', { spots, name, page, totalPages, deleteFaile : undefined });
-                });
-        }).catch(erreur => {
-            res.render("login");
-        });
-};
-
-exports.getCreateSpot = (req, res) => {
-    let token = res.app.locals.apiKey;
-    if (token!=undefined) {
-        axios.get(SkiApi + "/tokenInfo", {headers: {"Authorization": token}})
-        .then(resultat => {
-            let name = resultat.data.name;
-            res.render('spot_creation', {name: name});
-        })
-        .catch (error =>{
-            redirect("/");
-        });
-    }
-};
-
-exports.postCreateSpot = (req, res) => {
-    let token = res.app.locals.apiKey;
-    let description = req.body.description;
-    let name = req.body.name;
-    let address = req.body.address;
-    let difficulty = req.body.difficulty;
-
-    let coordinates =  req.body.coordinates;
-    let tabCoordinates = coordinates.split(",");
-    let numberTabCoordinates = tabCoordinates.map( i => Number(i));
-
-    axios.post(SkiApi + "/ski-spot", {
-
-            "description": description,
-            "name": name,
-            "address": address,
-            "difficulty": difficulty,
+    },
+    postUpdateSpot : (req, res) => {
+        let token = res.app.locals.apiKey;
+        let id = req.params.id;
+    
+        let infoUpdate = req.body
+        let coordinates = req.body.coordinates;
+        let tabCoordinates = coordinates.split(",");
+        let numberTabCoordinates = tabCoordinates.map( i => Number(i));
+    
+        let info = {
+            ...infoUpdate,
             "coordinates": numberTabCoordinates
-        }, 
-
-        {headers: {"Authorization": token}})
-
-        .then(resultat => {
-            res.redirect('/');
-        })
-        .catch(erreur => {
-            req.flash("error", `Impossible de créer le spot`);
-            res.redirect("/spots/create");
-        });
-};
-
-exports.getSpotInformation = (req, res) => {
-    let token = res.app.locals.apiKey;
-    let id = req.params.id;
-
-    axios.get(SkiApi + "/ski-spot/" + id, {
-
-            headers: {"Authorization": token}
-        })
-        .then(resultat => {
-            let info = resultat.data.skiSpot;
-            axios.get(SkiApi + "/tokenInfo", {headers: {"Authorization": token}})
-            .then(resultat => {
-                let name = resultat.data.name;
-                res.render('spot_information', {name: name, info:info});
-            });            
-        }).catch(erreur => {
-            res.redirect("/");
-        });
-};
-
-exports.deleteSpot = (req, res) => {
-    let token = res.app.locals.apiKey;
-    let id = req.params.id;
-
-    axios.delete(SkiApi + "/ski-spot/" + id, {
-
-            headers: {"Authorization": token}
-        })
-        .then(resultat => {
-            res.redirect("/");
-        }).catch(erreur => {
-            res.redirect("/");
-        });
-};
-
-exports.getUpdateSpot = (req, res) => {
-
-    let token = res.app.locals.apiKey;
-    let id = req.params.id;
-
-    axios.get(SkiApi + "/ski-spot/" + id,
-    
-           {headers: {"Authorization": token}})
-
-        .then(resultat => {
-            console.log(`resultat.data ${JSON.stringify(resultat.data)}`);
-            let info = resultat.data.skiSpot;
-
-            axios.get(SkiApi + "/tokenInfo", {headers: {"Authorization": token}})
-            .then(resultat => {
-                let name = resultat.data.name;
-                res.render('spot_update', {name: name,info:info});
-            });
-        })    
-        .catch(erreur => {
-             res.send(erreur);
-        });
-};
-
-exports.postUpdateSpot = (req, res) => {
-
-    let id = req.params.id;
-    let token = res.app.locals.apiKey;
-
-    let description = req.body.description;
-    let name = req.body.name;
-    let address = req.body.address;
-    let difficulty = req.body.difficulty;
-    let coordinates = req.body.coordinates;
-    let tabCoordinates = coordinates.split(",");
-    let numberTabCoordinates = tabCoordinates.map( i => Number(i));
-
-    let info = {
-        "description": description,
-        "name": name,
-        "address": address,
-        "difficulty": difficulty,
-        "coordinates": numberTabCoordinates
-    };
-    axios.put(SkiApi + "/ski-spot/" + id, 
-            info,
-    
-    { headers: {"Authorization": token}})
-    .then(resultat => {
-          res.redirect("/"); })
-    .catch(erreur => {
-        axios.get(SkiApi + "/tokenInfo", {headers: {"Authorization": token}})
-        .then(resultat => {
-            let name = resultat.data.name;
+        };
+        axios.put(SkiApi + "/ski-spot/" + id, info, { headers: {"Authorization": token}})
+        .then(() => {res.redirect("/"); })
+        .catch(() => {
             req.flash("error", `Une Erreur c'est produite lors de la modification du spot `);
-            res.render("spot_update", {name: name,info:info});
-        })
-        .catch(erreur => {
+            res.redirect("/spots/update/" + id);
+            });
+    },
+    deleteSpot : (req, res) => {
+        let token = res.app.locals.apiKey;
+        let id = req.params.id;
+    
+        axios.delete(SkiApi + "/ski-spot/" + id, {headers: {"Authorization": token}})
+            .then(() => res.redirect("/"))
+            
+            .catch(() => {
+            req.flash("error", `Le Spot n'a pu être supprimé `);
             res.redirect("/");
-        });
-    });
-};
-
-exports.error404 = (req, res) => {
-    res.render("error404");
+            });
+    },
+    error404 : (req, res) => {
+        res.render("error404");
+    },
 };
--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,26 @@ const express = require("express");
 const router = express.Router();
 const controllerRoute = require('./controllerRoute');
 
+router.get("/", controllerRoute.getSpots,controllerRoute.getProfil, controllerRoute.pageSpotsList);
+router.get("/:page", controllerRoute.getSpots,controllerRoute.getProfil, controllerRoute.pageSpotsList);
 
-router.get('/users/signup', controllerRoute.getSignup);
+router.get('/users/signup', controllerRoute.pageSignup);
 router.post("/users/signup", controllerRoute.postSignup);
+
 router.post("/users/login", controllerRoute.postLogin);
-router.get('/users/profil', controllerRoute.getProfil);
-router.get("/users/logout", controllerRoute.getDeconnexion);
 
+router.get('/users/profil', controllerRoute.getProfil, controllerRoute.pageProfil);
 
-router.get("/spots/create", controllerRoute.getCreateSpot);
+router.get("/spots/create", controllerRoute.pageCreateSpot);
 router.post("/spots/create", controllerRoute.postCreateSpot);
-router.delete("/spots/delete/:id", controllerRoute.deleteSpot);
-router.get("/spots/update/:id", controllerRoute.getUpdateSpot);
-router.put("/spots/update/:id", controllerRoute.postUpdateSpot);
-router.get('/spots/:id', controllerRoute.getSpotInformation);
 
-router.get('/', controllerRoute.getSpots);
-router.get('/:page', controllerRoute.getSpots);
+router.get('/spots/:id', controllerRoute.getSpotInformation, controllerRoute.pageInformation);
+
+router.get("/spots/update/:id",controllerRoute.getSpotInformation,controllerRoute.getProfil, controllerRoute.pageUpdate);
+router.put("/spots/update/:id", controllerRoute.postUpdateSpot);
+
+router.delete("/spots/delete/:id", controllerRoute.deleteSpot);
+router.get("/users/logout", controllerRoute.getDeconnecxion);
 
 router.get("*", controllerRoute.error404);
 
