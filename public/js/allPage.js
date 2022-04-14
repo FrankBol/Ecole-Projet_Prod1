344b314eb98f233976b3c140154865eab7afa440
Francis Bolduc <f.bolduc@outlook.com>
2022-04-14 11:05:07 -0400

    s

--- public/js/allPage.js
+++ public/js/allPage.js
@@ -1,4 +1,4 @@
-logo = () => C.assign("/");
+logo = () => window.location.assign("/");
 imgProfil = () => window.location.assign("/users/profil");
 
 let pagination = document.querySelectorAll("#pagination[href]");
@@ -19,18 +19,21 @@ for (var i = 0; i < pagination.length + 1; i++) {
     
 }
 
-// nextPrev => {
-//     for (var i = 0; i < pagination.length + 1; i++) {
-//         console.log(pagination.length);
-//         console.log(window.location.href);
-//         console.log(window.origin + "/" + i);
+nextPrev = () => {
+    for (var i = 0; i < pagination.length + 1; i++) {
+        console.log(pagination.length);
+        console.log(window.location.href);
+        console.log(window.origin + "/" + i);
         
-//         if(window.location.href == window.origin + "/" + i){
-//             console.log(window.location.href);
-//             console.log(window.origin + "/" + i);
-//             console.log(i);
-//             pagination[i-1].style="font-size: 2.5rem"
-//         }
-// }
+        if(window.location.href == window.origin + "/?" + i){
+            console.log(window.location.href);
+            console.log(window.origin + "/?" + i);
+            console.log(i);
+            console.log("ooooooooooooooooooooooooooooo");
+            pagination[i-1].style="font-size: 2.5rem";
+            window.location.assign("/");
 
-// }
+        }
+}
+
+}
